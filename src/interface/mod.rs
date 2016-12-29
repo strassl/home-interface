4d4a414387da165f4b0e51d9d831e0b4c3aa2e5b
Simon Strassl <stuff@sigmoid.at>
2016-12-29 09:06:05 +0100

    Add simple fake rest api

--- src/interface/mod.rs
+++ src/interface/mod.rs
@@ -5,5 +5,45 @@ extern crate iron;
 extern crate serde_json;
 extern crate valico;
 
-include!(concat!(env!("OUT_DIR"), "./schema.in.rs"));
+include!(concat!(env!("OUT_DIR"), "/schema.rs"));
 
+use self::valico::json_dsl;
+use self::hyper::status::StatusCode;
+use self::rustless::{
+    Application, Api, Nesting, Versioning
+};
+use self::rustless::json::ToJson;
+
+pub fn create_application() -> Application {
+    let api = Api::build(|api| {
+        // Specify API version
+        api.prefix("api");
+
+        // Create API for chats
+        api.mount(Api::build(|status_api| {
+
+            status_api.after(|client, _params| {
+                client.set_status(StatusCode::Ok);
+                Ok(())
+            });
+
+            // Add namespace
+            status_api.namespace("status", |status_ns| {
+                status_ns.get("", |endpoint| {
+                    endpoint.desc("Get current status");
+
+                    endpoint.handle(|client, params| {
+                        let status = Status {
+                            r: 255,
+                            g: 254,
+                            b: 253,
+                        };
+                        client.json(&status.to_json())
+                    })
+                });
+            });
+        }));
+    });
+
+    Application::new(api)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+extern crate iron;
+mod interface;
+
+use interface::create_application;
+
 fn main() {
-    println!("Hello world!");
+    let app = create_application();
+
+    println!("Rustless server started!");
+    println!("On 8000");
+    iron::Iron::new(app).http("0.0.0.0:8000").unwrap();
 }
